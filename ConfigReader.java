7cf71ce52b86ae385a6d825151facf50effdcc3d
seema6104 <54417967+seema6104@users.noreply.github.com>
2021-01-31 22:31:59 -0500

    Update ConfigReader.java

--- ConfigReader.java
+++ ConfigReader.java
@@ -1,17 +1,15 @@
 package com.qa.util;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
-
 public class ConfigReader {
-    private Properties prop;
+     private Properties prop;
     /**
-     * 
+     * https://www.toolsqa.com/cucumber-video-tutorials/
      * @return
      */
-	public Properties init_prop()  {
+	 public Properties init_prop()  {
 		prop= new Properties();
 		
 		try {
@@ -24,9 +22,23 @@ public class ConfigReader {
 			e.printStackTrace();
 		}
 		return prop;
-		
-	}
-	
+	  }
+	   public String getUrl(String url)
+	 {
+		String samUrl=prop.getProperty(url+"URL");
+		// String samUrl="https://sam.cengage.com/App/login?ReturnUrl=%2f";
+		return samUrl;
+	  }
+     public String getUername(String user)
+	 {
+	    String username=prop.getProperty(user+"student.userName");
+		return username;
+	 }
+	  public String getPassword(String pwd)
+	    {
+	   	 String password=prop.getProperty(pwd+"student.password");
+			return password;
+	    }
 	
 	
 

f30a9484d02f557d912605055bd9be18a6d1d8cf
seema6104 <54417967+seema6104@users.noreply.github.com>
2021-01-31 22:35:35 -0500

    Update LoginSteps.java

--- LoginSteps.java
+++ LoginSteps.java
@@ -1,53 +1,73 @@
 package stepdefinitions;
-import org.junit.Assert;
 import com.pages.LaunchContentPlayer;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import com.pages.LoginPage;
 import com.qa.facory.DriverFactory;
+import com.qa.util.ConfigReader;
 import io.cucumber.java.en.*;
-public class SamSteps extends BaseClass{
-	
-	@Given("User Launch Chrome browser")
-	public void user_launch_chrome_browser() {
-	System.out.println("Sam Login Steps class");
-	
+public class LoginSteps{
+	private LoginPage login= new LoginPage(DriverFactory.getDriver());
+	private LaunchContentPlayer cpPage = new LaunchContentPlayer(DriverFactory.getDriver());
+	private ConfigReader config;
+	public Logger logger;
+	/**
+	 * @param url
+	 * @throws InterruptedException
+	 */
+	@When("^User Launch SAM URL(.*)$")
+	public void user_launch_sam_url(String url) throws InterruptedException{
+		 config=new ConfigReader();
+		 logger=Logger.getLogger("Sam_project_BDD_Cucumber");
+		 PropertyConfigurator.configure("src\\test\\resources\\log4j.properties"); //Added Logger
+		 logger.info("************ Launching Browser ***************");
+		 Thread.sleep(4000);
+		 //DriverFactory.getDriver().get(url);
+		 DriverFactory.getDriver();
+		 Thread.sleep(5000);
+		 config.getUrl(url);
+		 DriverFactory.getDriver();
+	      }
+	/**
+	 * 
+	 * @param user
+	 * @param pwd
+	 */
+    @When("^User Login as (.*)$")
+	public void user_login_as(String user, String pwd)
+    {
+		 config.getUername(user);
+    	 config.getPassword(pwd);
 	}
-
-	@When("User Launch SAM URL {string}")
-	public void user_launch_sam_url(String url) {
-		
-		
-	}
-
-	@When("User enters Email as {string} and Password as {string}")
-	public void user_enters_email_as_and_password_as(String uName, String pwd) {
-		login.enterUsername(uName);
-		login.enterPassword(pwd);
-	}
-
+     /**
+      * 
+      * @throws Exception
+      */
 	@When("Clicks on login")
-	public void clicks_on_login() {
-		  
+	public void clicks_on_login() throws Exception {
+		login.clickOnLogin();
+		logger.info("*********** New session link ******************");
+    	login.newSessionOKBTN();
+    	logger.info("*********** Enter key later *******************");
+    	login.enterKeyLaterButton();
+    	Thread.sleep(4000);
 	}
 
 	@Then("Student user can see Dashboard")
-	public void student_user_can_see_dashboard() throws InterruptedException {
-		login.clickOnLogin();
-		System.out.println("Now Clicking on New session link");
-    	login.newSessionOKBTN();
-    	System.out.println("Clicking on Enter key later");
-    	login.enterKeyLaterButton();
-    	Thread.sleep(4000);
-		String actualTitle= driver.getTitle();
-    	System.out.println("actualTitle after Login: " +actualTitle);
-		Assert.assertEquals(actualTitle, "SAM - Activity Calendar");
-		//login.samLogoPage();
-	}
+	public void student_user_can_see_dashboard(){
+		try {
+		String actualTitle= login.getPageTitle();
+		logger.info("Title of the page : " + actualTitle);
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	 }
 
    // Launch content player
 
 	@Then("Student user Navigates to Activity List")
 	public void student_user_navigates_to_activity_list() {
-		cpPage= new LaunchContentPlayer(driver); 
 		cpPage.clickOnActivityListTab();
 		cpPage.selectCourseTab();
 	}
@@ -63,5 +83,8 @@ public class SamSteps extends BaseClass{
 	public void retake_of_the_assignment_should_not_get_reduced() {
 		
 	}
+	
+	
+
 
 }
